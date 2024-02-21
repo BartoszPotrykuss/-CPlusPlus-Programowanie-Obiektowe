b791bf05c79b90892813e6d02826c5ce73e8d0c8
2fT <2ft@s236.lan>
2024-02-21 10:11:43 +0100

    Dokonczenie zad.3 klasy zaprzyjaznione

--- zad.3 funkcje i klasy zaprzyjaznione/main.cpp
+++ zad.3 funkcje i klasy zaprzyjaznione/main.cpp
@@ -10,6 +10,11 @@ public:
     friend class Pracownik;
     friend class Uczen;
 
+    Osoba(string imie, string nazwisko) {
+        this->imie = imie;
+        this->nazwisko = nazwisko;
+    }
+
 };
 
 class Pracownik {
@@ -17,24 +22,49 @@ private:
     string stanowisko;
 
 public:
-    void setImie(string imie) {
-        imie = imie;
+    Pracownik(string stanowisko) {
+        this->stanowisko = stanowisko;
     }
-    string getImie() {
-        return imie;
+
+    void wyswietlDane(Osoba& osoba) {
+        cout << "Dane pracownika: " << endl <<
+        "Imie: " << osoba.imie << endl <<
+        "Nazwisko: " << osoba.nazwisko << endl <<
+        "Stanowisko: " << stanowisko << endl;
     }
 };
 
 class Uczen {
 private:
     string klasa;
+
+public:
+    Uczen(string klasa) {
+        this->klasa = klasa;
+    }
+
+    void wyswietlDane(Osoba& osoba) {
+        cout << "Dane ucznia: " << endl <<
+        "Imie: " << osoba.imie << endl <<
+        "Nazwisko: " << osoba.nazwisko << endl <<
+        "Klasa: " << klasa << endl;
+    }
 };
 
 int main()
 {
-    Pracownik p1 = Pracownik();
-    p1.setImie("Jan");
-    p1.getImie();
+    Osoba pracownik = Osoba("Jan", "Kowalski");
+    Pracownik p1 = Pracownik("nauczyciel");
+    p1.wyswietlDane(pracownik);
+    cout << endl;
+
+    Osoba uczen = Osoba("Piotr", "Nowak");
+    Uczen u1 = Uczen("1a");
+    u1.wyswietlDane(uczen);
+
+
+
+
 
     return 0;
 }
